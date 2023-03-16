755791defed159d1c22fe72f95f8508f349b51a6
Sebastian Lopez-Marcano <slopezmarcano@gmail.com>
2023-03-16 10:57:11 +1000

    README GIF and 003_dynamic_map 1. new dynamic map in 003_dynamic_map 2. GIF that changes the logan road shapefile depending on the traffic level 3. README changes and yyy_theme_setup

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Brisbane Traffic in 2018
+# 🚔 Brisbane Traffic in 2018
 Brisbane is a rapidly growing metropolitan city in Australia, and in 2032 they will host the olympics. Brisbane's traffic has been getting worse throughout the years and road infrastucture cannot withstand the current and project traffic volume. 
 
 I wanted to understand how Brisbane traffic was in previous years, and then compare it with current data. Data for 2021, 2022 or even 2023 is yet to be made available by the Open Data team at Queensland Department of Transport. I was able to access the 2018 data, specifically the peak volume for AM and PM across all key QLD corridors. 
@@ -15,3 +15,10 @@ Contrary to above, in August you can see how the PM peak is higher than the AM p
 
 Overall, there are spikes in car volume throughout the year, but it is very cler than traffic in Logan Road decreases from October until January (summer holidays). 
 
+## 🚦2018 BNE Traffic Visualisation of Logan Road
+
+Using the Open Data from the Queensland Government I have created a short GIF that shows the Peak volume during AM hours for Logan Road across months. Every month depending on the traffic level, the colours will be green, for low volume; orange for medium volume and red for high volume. I also added the suburbs in which Logan Road transverses. 
+
+<p align="center">
+<img src="outputs/bne_2018_visualisation.gif" width='550'/>
+</p>
--- scripts/003_dynamic_map.R
+++ scripts/003_dynamic_map.R
@@ -3,16 +3,17 @@
 
 #--LIBRARIES--#
 library(tidyverse)
-library(ggtext) #add and modify text to ggpplot
-library(showtext) #fonts
-font_add_google("Lato")
-showtext_auto()
-library(ggmap)
+#library(ggtext) #add and modify text to ggpplot
+#library(showtext) #fonts
+#font_add_google("Lato")
+#showtext_auto()
+#library(ggmap)
 library(sp)
 library(sf)
 library(gganimate)
-library(gifski)
-library(magick)
+library(transformr)
+#library(gifski)
+#library(magick)
 
 #--READ STREET SHAPEFILES--#
 logan_road <- st_read("assets/spatial/QSC_Extracted_Data_20230307_081354809000-55796/Queensland_roads_and_tracks.shp") %>%
@@ -31,22 +32,42 @@ brisbane_cut <- st_intersection(logan_road_buffer, brisbane)
 #--CROP BRISBANE USING THE INTERSECTION--#
 brisbane_cropped <- st_crop(brisbane, brisbane_cut)
 
-#--READ DATASET--#
+#--READ TRAFFIC DATASET--#
 traffic <- read_csv('assets/df_cleaned.csv')
 
+#--MERGE LOGAN ROAD SHAPEFILE WITH TRAFFIC DATA--#
 logan_road_bne <- merge(logan_road, traffic, by.x = "ROAD_NAME_", by.y = "corridor")
 
 
-# Create a categorical variable based on the am_peak values
-logan_road_bne$category <- cut(logan_road_bne$am_peak, 
+#--CREATE CATEGORICAL VARIABLE BASED ON AM_PEAK--#
+logan_road_bne$traffic_level <- cut(logan_road_bne$am_peak, 
                                breaks = quantile(logan_road_bne$am_peak, probs = c(0, 0.25, 0.75, 1)),
-                               labels = c("low", "medium", "high"),
+                               labels = c("Low Volume", "Medium Volume", "High Volume"),
                                include.lowest = TRUE)
 
-p <- ggplot() +
-  geom_sf(data = brisbane_cropped) +
-  geom_sf(data = logan_road_bne, aes(color = category)) +
-  coord_sf() +
-  labs(title = "Month: {frame_time}")
+#--CONVERT MONTH TO FACTOR AND ASSIGN ORDER--#
+logan_road_bne$Month <- factor(logan_road_bne$Month, levels = c("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"))
 
-#TODO: #1 develop code to convert the ggplot into a GIF
+#--DEFINE THE TRAFFIC COLOURS TO EACH CATEGORY--#
+traffic_colors <- c("Low Volume" = "green", "Medium Volume" = "orange", "High Volume" = "red")
+
+#--GGPLOT AND GGANIMATE--#
+p <- ggplot() +
+  geom_sf(data = brisbane_cropped, colour="#585656", fill="grey98") +
+  geom_sf(data = logan_road_bne, aes(color = traffic_level)) +
+  scale_color_manual(values = traffic_colors) +
+  geom_sf_text(data=brisbane_cropped, aes(label = LOCALITY), size=2, color ="black")+
+  #coord_sf() +
+  labs(x= '',
+      y='',
+      title = 'Month: {closest_state}',
+      subtitle='',
+      caption = "Visualisation by Sebastian Lopez Marcano 🀰") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
+  transition_states(logan_road_bne$Month, transition_length = 1)
+
+#--ANIMATION--#
+animate(p)
+
+#--ANIMATION SAVE--#
+anim_save('outputs/bne_2018_visualisation.gif', animation = last_animation())
